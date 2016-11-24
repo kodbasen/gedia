389c9ebee234c11ac39ebb7611dc0b0a15a7129c
Lars Mogren <lars.mogren@gmail.com>
2016-11-24 23:00:51 +0100

    A clasic Hello World script

---
+++ gedia.groovy
@@ -0,0 +1,13 @@
+@Grab('io.ratpack:ratpack-groovy:1.3.3')
+@Grab('ch.qos.logback:logback-classic:1.1.7')
+@Grab('ch.qos.logback:logback-core:1.1.7')
+
+import static ratpack.groovy.Groovy.ratpack
+
+ratpack {
+  handlers {
+    get {
+      render "Hello World"
+    }
+  }
+}
